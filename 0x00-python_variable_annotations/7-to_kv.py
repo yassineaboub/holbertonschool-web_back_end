51c7110d8bb3b54bfae55e7df2daf91ccfabde90
root <yassineaboub@live.fr>
2020-10-22 15:14:46 +0000

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """string int float to tuple function"""
+from typing import Union, Tuple
 
 
-from typing import Callable, Iterator, Union, Optional, List, Tuple
-
-def to_kv(k : str,v: Union[int, float]) -> Tuple[str,float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    takes a string k and an int
+    OR float v as arguments
+    """
     return (k, v**2)
