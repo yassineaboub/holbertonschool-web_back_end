60bb30aa07f5902b06ae96951a335b7d0968b127
root <yassineaboub@live.fr>
2020-10-22 15:24:04 +0000

    100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """first element of a sequence"""
-
-
 from typing import Callable, Iterator, Union, Optional, List, Sequence, Any
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Duck typing
+    """
     if lst:
         return lst[0]
     else:
