24fe2ba88c1c5fba5de86a35ad1a169083ce256f
root <yassineaboub@live.fr>
2020-10-22 15:17:39 +0000

    8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 """complex types functions"""
-
-
 from typing import Callable, Iterator, Union, Optional, List
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    def ret(i:float) -> float:
+    """
+    takes a float multiplier as argument
+    """
+    def ret(i: float) -> float:
+        """
+        take a float i as argument
+        """
         return i * multiplier
     return ret
