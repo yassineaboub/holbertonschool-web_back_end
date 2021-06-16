113ab532057f5982f10380dac49e50b38f9e788e
root <yassineaboub@live.fr>
2021-06-16 22:36:08 +0000

    1-promise.js

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,9 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise(function(resolve, reject){
-    if (success == true) {
-      resolve({ status: 200, body: 'Success' });
-    } else if ( success == false ) {
-      reject(Error('The fake API is not working currently'));
-    }
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else reject(Error('The fake API is not working currently'));
   });
 }
